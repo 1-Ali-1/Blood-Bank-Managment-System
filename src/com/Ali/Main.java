71d5fcfed1cc7ab9ff2bb9cdb797bff596ba6518
unknown <alifarhad.engineer@gmail.com>
2021-10-25 20:28:40 +0300

    half way through

--- src/com/Ali/Main.java
+++ src/com/Ali/Main.java
@@ -7,6 +7,7 @@ import com.Ali.model.storing_donor_info.ListOfDonors;
 import com.Ali.model.storing_recipient_registration.ListOfRecipient;
 import com.Ali.model.storing_recipient_registration.RecipientRegistration;
 import com.Ali.Storage_From_File_To_Collection.WriteToFile;
+import com.Ali.view.Interface;
 
 import java.io.BufferedReader;
 
@@ -18,96 +19,8 @@ public class Main {
 
     public  static void main(String[]args){
 
-        ListOfDonors listOfDonors=new ListOfDonors();
-        ListOfRecipient listOfRecipient=new ListOfRecipient();
-        InsertingAndShowingData insertingAndShowingData=new InsertingAndShowingData();
-
-        int i;
-
-        try {
-            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println(" * * * * * Welcome To Blood Bank * * * * *");
-            System.out.println(" please choose with objective you want:");
-            System.out.println(" enter (1) for registering new Donor");
-            System.out.println(" enter (2) for registering new Recipient");
-            System.out.println(" enter (3) for read Donor details");
-            System.out.println(" enter (4) for read Recipient details ");
-            System.out.println(" enter (0)  for exit the program");
-
-            i=Integer.parseInt(stdin.readLine());
-
-            while(i!=0){
-
-
-
-
-                if(i==1){
-                    Donor_Registration dr=new Donor_Registration();
-                    insertingAndShowingData.InsertForDonor(dr);
-                    // writing to file
-                    WriteToFile writeToFile=new WriteToFile();
-                    writeToFile.writeObjectToFile(dr,"Donor details.txt");
-
-                    System.out.println("Done Registering !!");
-                }
-
-                else if(i==2){
-
-                    RecipientRegistration ur=new RecipientRegistration();
-                   insertingAndShowingData.InsertForRecipient(ur);
-
-                 // writing to file
-                    WriteToFile writeToFile=new WriteToFile();
-                    writeToFile.writeObjectToFile(ur,"Recipient details.txt");
-
-                    System.out.println("Done Registering !!");
-
-                }
-
-
-                else if(i==3){
-
-
-                   // read from file
-                    ReadFromFile readFromFile = new ReadFromFile();
-                    readFromFile.readFromFileForDonor(listOfDonors);
-
-
-                 //showing donor data
-                insertingAndShowingData.ShowingDonorData(listOfDonors);
-
-
-
-
-                }
-
-                else if(i==4){
-
-
-                   // read from file
-
-                    ReadFromFile readFromFile=new ReadFromFile();
-                    readFromFile.readFromFileForRecipient(listOfRecipient);
-
-
-                       //showing recipient data
-                    insertingAndShowingData.ShowingRecipientData(listOfRecipient);
-
-                }
-
-                System.out.println("*********Welcome To Blood Bank***********");
-                System.out.println(" enter (1) for registering new donor");
-                System.out.println(" enter (2) for registering new Recipient");
-                System.out.println(" enter (3) for read Donor");
-                System.out.println(" enter (4) for read  Recipient");
-                System.out.println(" enter (0) for exit the program");
-
-                i=Integer.parseInt(stdin.readLine());
-
-            }
-
-        } catch (Exception ex) { System.out.println(ex.getMessage());}
-
+        Interface viewController = new Interface();
+        viewController.ui();
     }
 
 
--- src/com/Ali/view/Interface.java
+++ src/com/Ali/view/Interface.java
@@ -1,7 +1,119 @@
 package com.Ali.view;
 
+import com.Ali.Storage_From_File_To_Collection.ReadFromFile;
+import com.Ali.Storage_From_File_To_Collection.WriteToFile;
+import com.Ali.model.InsertingAndShowingData;
+import com.Ali.model.storing_donor_info.Donor_Registration;
+import com.Ali.model.storing_donor_info.ListOfDonors;
+import com.Ali.model.storing_recipient_registration.ListOfRecipient;
+import com.Ali.model.storing_recipient_registration.RecipientRegistration;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Interface {
 
+
+    public void ui(){
+        ListOfDonors listOfDonors=new ListOfDonors();
+        ListOfRecipient listOfRecipient=new ListOfRecipient();
+        InsertingAndShowingData insertingAndShowingData=new InsertingAndShowingData();
+
+        int i;
+
+        try {
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(" * * * * * Welcome To Blood Bank * * * * *");
+            System.out.println(" please choose with objective you want:");
+            System.out.println(" enter (1) for registering new Donor");
+            System.out.println(" enter (2) for registering new Recipient");
+            System.out.println(" enter (3) for read Donor details");
+            System.out.println(" enter (4) for read Recipient details ");
+            System.out.println(" enter (0)  for exit the program");
+
+            i=Integer.parseInt(stdin.readLine());
+
+            while(i!=0){
+
+
+
+
+                if(i==1){
+                    Donor_Registration dr=new Donor_Registration();
+                    insertingAndShowingData.InsertForDonor(dr);
+                    // writing to file
+                    WriteToFile writeToFile=new WriteToFile();
+                    writeToFile.writeObjectToFile(dr,"Donor details.txt");
+
+                    System.out.println("Done Registering !!");
+                }
+
+                else if(i==2){
+
+                    RecipientRegistration ur=new RecipientRegistration();
+                    insertingAndShowingData.InsertForRecipient(ur);
+
+                    // writing to file
+                    WriteToFile writeToFile=new WriteToFile();
+                    writeToFile.writeObjectToFile(ur,"Recipient details.txt");
+
+                    System.out.println("Done Registering !!");
+
+                }
+
+
+                else if(i==3){
+
+
+                    try{ ReadFromFile readFromFile = new ReadFromFile();
+                        readFromFile.readFromFileForDonor(listOfDonors);
+
+                    }catch (Exception e){
+                        System.out.println("there is no data ");
+                    }
+                    // read from file
+
+
+                    //showing donor data
+                    insertingAndShowingData.ShowingDonorData(listOfDonors);
+
+
+
+
+                }
+
+                else if(i==4){
+
+
+                    // read from file
+
+                    try{   ReadFromFile readFromFile=new ReadFromFile();
+                        readFromFile.readFromFileForRecipient(listOfRecipient);
+
+
+                    }catch (Exception e){
+                        System.out.println("there is no data ");
+                    }
+
+                    //showing recipient data
+                    insertingAndShowingData.ShowingRecipientData(listOfRecipient);
+
+                }
+
+                System.out.println("*********Welcome To Blood Bank***********");
+                System.out.println(" enter (1) for registering new donor");
+                System.out.println(" enter (2) for registering new Recipient");
+                System.out.println(" enter (3) for read Donor");
+                System.out.println(" enter (4) for read  Recipient");
+                System.out.println(" enter (0) for exit the program");
+
+                i=Integer.parseInt(stdin.readLine());
+
+            }
+
+        } catch (Exception ex) { System.out.println(ex.getMessage());}
+
+    }
 
 
 }
